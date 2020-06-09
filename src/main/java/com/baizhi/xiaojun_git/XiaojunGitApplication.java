a03e2623a9f5fd182be4cf0d48134dbe26d882a9
3042023089@qq.com <3042023089@qq.com>
2020-06-09 22:14:41 +0800

    第一次集成练习

---
+++ src/main/java/com/baizhi/xiaojun_git/XiaojunGitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.xiaojun_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaojunGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaojunGitApplication.class, args);
+    }
+
+}
